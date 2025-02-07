c03e33727e8b8667c69e7f97f531717aa91c991b
divyansh-pathak129 <divyansh.pathak129@gmail.com>
2025-02-08 01:29:14 +0530

    Server initiation

--- server/app.js
+++ server/app.js
@@ -3,6 +3,16 @@ const http = require('http');
 const express  = require('express');
 const { Server } = require('socket.io');
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://divyanshpathak129:<db_password>@workify-data.hzaze.mongodb.net/?retryWrites=true&w=majority&appName=workify-data";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
 const app = express()
 app.use(cors());
@@ -16,3 +26,14 @@ const io = new Server(server, {
     }
 });
 
+io.on('connection', (socket) => {
+
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on('disconnect', () => {
+        console.log(`User Disconnected: ${socket.id}`);
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("Server is Running on " + PORT));
